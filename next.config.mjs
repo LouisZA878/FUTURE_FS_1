bab72490a5b8b8c3b3d18c657ef13989fdeb4692
chatsworthjumpingcastlehire <LouisZA878>
2026-05-15 16:19:29 +0200

    Update - removed unncessary code

--- next.config.mjs
+++ next.config.mjs
@@ -2,9 +2,9 @@
 const nextConfig = {
   /* config options here */
   output: "export",
-  images: { unoptimized: true },
-  basePath: "/FUTURE_FS_1", //
-  assetPrefix: "/FUTURE_FS_1/",
+  // images: { unoptimized: true },
+  // basePath: "/FUTURE_FS_1", //
+  // assetPrefix: "/FUTURE_FS_1/",
 };
 
 export default nextConfig;
