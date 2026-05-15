c13eee6ec7eb902bf22fcaf41f93bb28de3cc090
LouisZA878 <LouisZA878>
2026-05-15 19:19:12 +0200

    Update - improved mobile experience

--- app/(components)/contact.js
+++ app/(components)/contact.js
@@ -1,43 +1,17 @@
-"use client";
+import ContactForm from "./contact/contactForm";
 
-import { useRef } from "react";
-import emailjs from "@emailjs/browser";
-
-const Contact = ({ template, service, publicKey }) => {
-  const form = useRef();
-
-  const sendEmail = (e) => {
-    e.preventDefault();
-
-    emailjs.sendForm(service, template, form.current, publicKey).then(
-      () => {
-        alert("Message sent!");
-      },
-      (error) => {
-        console.log(error.text);
-        alert("Failed to send");
-      },
-    );
-
-    e.target.reset();
-  };
-
+const Contact = () => {
+  const { TEMPLATE_ID, SERVICE_ID, PUBLIC_KEY } = process.env;
   return (
     <section className="contact" id="contact">
       <h2 className="title">
         <span>Contact Form</span>
       </h2>
-      <form ref={form} onSubmit={sendEmail}>
-        <div className="inputs">
-          <input type="text" name="user_name" placeholder="Name" required />
-
-          <input type="email" name="user_email" placeholder="Email" required />
-        </div>
-
-        <textarea name="message" placeholder="Message" required />
-
-        <button type="submit">Submit</button>
-      </form>
+      <ContactForm
+        template={TEMPLATE_ID}
+        service={SERVICE_ID}
+        publicKey={PUBLIC_KEY}
+      />
     </section>
   );
 };
---
+++ app/(components)/contact/contactForm.js
@@ -0,0 +1,40 @@
+"use client";
+
+import { useRef } from "react";
+import emailjs from "@emailjs/browser";
+
+const ContactForm = () => {
+  const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs.sendForm(service, template, form.current, publicKey).then(
+      () => {
+        alert("Message sent!");
+      },
+      (error) => {
+        console.log(error.text);
+        alert("Failed to send");
+      },
+    );
+
+    e.target.reset();
+  };
+
+  return (
+    <form ref={form} onSubmit={sendEmail}>
+      <div className="inputs">
+        <input type="text" name="user_name" placeholder="Name" required />
+
+        <input type="email" name="user_email" placeholder="Email" required />
+      </div>
+
+      <textarea name="message" placeholder="Message" required />
+
+      <button type="submit">Submit</button>
+    </form>
+  );
+};
+
+export default ContactForm;
