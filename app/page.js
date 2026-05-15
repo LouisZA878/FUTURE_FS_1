547555534a153d5a2dee7e59db31ab4c39752522
LouisZA878 <LouisZA878>
2026-05-15 17:27:29 +0200

    bug fix

--- app/page.js
+++ app/page.js
@@ -18,7 +18,7 @@ const Projects = dynamic(() => import("./(components)/projects"), {
 export default function Home() {
   const { TEMPLATE_ID, SERVICE_ID, PUBLIC_KEY } = process.env;
 
-  console.log(TEMPLATE_ID);
+  console.log(TEMPLATE_ID, SERVICE_ID);
 
   return (
     <>
